88ad359a9ef535ac73445e5b6b7eb0c92f5f345c
Jiefeng Li <xerath7d0@gmail.com>
2024-01-23 07:30:45 -0500

    store result in map and dump to json

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use regex::Regex;
-use std::{fs,env};
+use std::{fs,env,collections::HashMap};
 use walkdir::WalkDir;
 use tiny_keccak::{Hasher,Keccak};
 fn main() {
     let re = Regex::new(r#"revertReasonIfDebugFunction\(\"(.?*)\"\)"#).expect("unable to build new regex");
     let args : Vec<String> = env::args().collect();
     assert!(args.len()>=2);
+    let mut raintable = HashMap::new();
     let solidity_repo_path = &args[1];
     for entry in WalkDir::new(solidity_repo_path).into_iter().filter_map(|e| e.ok()){
         let path = entry.into_path();
@@ -30,8 +31,10 @@ fn main() {
             let hash_hex = out_put.iter().map(|byte| format!("{:02x}",byte)).collect::<String>();
             println!("Revert function call found in {:?}", &path);
             println!("{:?} : {:}",revert_reason,hash_hex);
-
+            raintable.insert(hash_hex, revert_reason.to_string());
         }
     }
+    let json = serde_json::to_string_pretty(&raintable).expect("unable to dump raintable to json");
+    fs::write("./out.json", &json).expect("Unable to write result into file");
 
 }
